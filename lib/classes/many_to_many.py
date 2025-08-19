63a66bb84758e2a6e7f287ba7952999fad1a6434
irene29-hub <muemarenee@gmail.com>
2025-08-19 14:49:51 +0300

    add sqlalchemy-code-challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,95 @@
 class Article:
+    all = []
+    
     def __init__(self, author, magazine, title):
+        self._title = title
         self.author = author
         self.magazine = magazine
-        self.title = title
-        
+        Article.all.append(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if hasattr(self, '_title'):
+            return
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if hasattr(self, '_name'):
+            return
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        articles = self.articles()
+        return list(set([magazine.category for magazine in self.magazines()])) if articles else None
 
 class Magazine:
+    all = []
+    
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        self._name = name
+        self._category = category
+        Magazine.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        pass
+        articles = self.articles()
+        return [article.title for article in articles] if articles else None
 
     def contributing_authors(self):
-        pass
+        counts = {}
+        for article in self.articles():
+            counts[article.author] = counts.get(article.author, 0) + 1
+        result = [author for author, count in counts.items() if count > 2]
+        return result if result else None
+    
+    @classmethod
+    def top_publisher(cls):
+        if not Article.all:
+            return None
+        counts = {}
+        for article in Article.all:
+            counts[article.magazine] = counts.get(article.magazine, 0) + 1
+        return max(counts, key=counts.get)
